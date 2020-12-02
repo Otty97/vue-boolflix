378105736a59997d12d85a26a2f996ef5d6b5d52
Othman Taik <taikothman1997@gmail.com>
2020-12-02 16:35:26 +0100

    Milestone 1
    need some layout fix

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-let myApp = new Vue ({
-	el:'#root',
-	data:{
-		library:[],
-	},
-	mounted: function(){
-			axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((result)=> {
-				this.library = result.data.response
-			});
-
-	}
-})
+const boolflixApp = new Vue({
+  el: '#root',
+  data: {
+    myAPI: 'c529733402bff4adb7705c88539dafa7',
+    library: [],
+    userInput: ''
+   },
+   methods:{
+    search: function(){
+         axios.get('https://api.themoviedb.org/3/search/movie',
+      {
+				params: {
+          api_key: this.myAPI,
+          query: this.userInput
+        }
+      }).then(result => this.library = result.data.results);
+    }
+   },
+});
