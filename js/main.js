67c5396006db5814a2020861d4cf2add48a5ba2d
Othman Taik <taikothman1997@gmail.com>
2020-12-09 11:14:41 +0100

    Completed 2.0

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
-const boolflixApp = new Vue({
-  el: '#root',
-  data: {
-    myAPI: 'c529733402bff4adb7705c88539dafa7',
-    library: [],
-    userInput: ''
+const app = new Vue({
+   el: '#root',
+   data: {
+      myAPI: 'c529733402bff4adb7705c88539dafa7',
+      films: [],
+      tvSeries: [],
+      userInput: '',
    },
    methods:{
-    search: function(){
+      search: function(){
          axios.get('https://api.themoviedb.org/3/search/movie',
-      {
-				params: {
-          api_key: this.myAPI,
-          query: this.userInput
-        }
-      }).then(result => this.library = result.data.results);
-    }
-   },
-});
+            {params: {api_key: this.myAPI, query: this.userInput}}).then(result => this.films = result.data.results);
+         axios.get('https://api.themoviedb.org/3/search/tv',
+            {params: {api_key: this.myAPI, query: this.userInput}}).then(result => this.tvSeries = result.data.results)
+      },
+      flagError: function(event){
+         event.target.src = 'img/svg/flag_404.svg';
+      },
+      imgError: function(event){
+         event.target.src = 'img/notfound.jpg'
+      },
+      starCounter: function(media){
+         let vote = Math.ceil(media.vote_average / 2);
+         return vote;
+      }
+   }
+})
